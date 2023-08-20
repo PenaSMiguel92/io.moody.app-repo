a06fcdcd826a499f5c6f5b9661c6233aa63660e2
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-20 09:58:06 -0700

    IDataAccessObject interface

--- src/main/java/io/moody/app/IDataAccessObject.java
+++ src/main/java/io/moody/app/IDataAccessObject.java
@@ -1,5 +1,9 @@
 package io.moody.app;
 
 public interface IDataAccessObject<T> {
-    
+    //crud - create, read, update, delete data from database;
+    public boolean createRecord(T data);
+    public T readRecord(int id);
+    public boolean updateRecord(T data);
+    public boolean deleteRecord(int id);
 }
