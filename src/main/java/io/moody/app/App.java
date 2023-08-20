1be426b33f6dc527531c9e7ee9d8f036eedeb0f9
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-20 08:20:13 -0700

    init

---
+++ src/main/java/io/moody/app/App.java
@@ -0,0 +1,13 @@
+package io.moody.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
