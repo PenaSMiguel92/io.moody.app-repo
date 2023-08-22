6cc8612c7aab9682e4bdec3343d796e23f9f1933
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-22 13:08:28 -0700

    Moodlet class implemented

--- src/main/java/io/moody/app/Moodlet.java
+++ src/main/java/io/moody/app/Moodlet.java
@@ -1,11 +1,65 @@
 package io.moody.app;
 
+import java.util.Objects;
+
 public class Moodlet {
-    int id;
-    int value;
-    String name;
+    private static int _id;
+    private int id;
+    private int value;
+    private String name;
 
     Moodlet(String name) {
         this.name = name;
+        this.id = _id++;
+    }
+
+    int getId() {
+        return this.id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    int getValue() {
+        return this.value;
+    }
+
+    void setValue(int value) {
+        this.value = value;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this)
+            return true;
+
+        if (this.getClass() != other.getClass())
+            return false;
+
+        Moodlet otherObj = (Moodlet) other;
+
+        if (otherObj.id == this.id)
+            return true;
+
+        return otherObj.name == this.name && otherObj.value == this.value;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.value);
+    }
+
+    @Override
+    public String toString() {
+        return "Moodlet: " + this.id + "\n Name: " + this.name + "\n Value: " + this.value;
     }
 }
