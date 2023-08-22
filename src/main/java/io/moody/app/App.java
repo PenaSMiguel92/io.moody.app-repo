4c76251200fcb5d20a5cdd48d730dba949666eca
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-21 22:54:28 -0700

    additional functionality for userdao

--- src/main/java/io/moody/app/App.java
+++ src/main/java/io/moody/app/App.java
@@ -1,12 +1,21 @@
 package io.moody.app;
 
 import java.util.*;
+import java.io.IOException;
 import java.sql.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        
+        try (Connection connection = DatabaseUtil.getConnection();) {
+            UserDAO userDAO = new UserDAO(connection);
+            User newUser = new User("blehman3000", "suckthis@notanemail.com", "lickme123");
+            userDAO.createRecord(newUser);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/io/moody/app/UserDAO.java
+++ src/main/java/io/moody/app/UserDAO.java
@@ -1,8 +1,24 @@
 package io.moody.app;
 
+import java.sql.*;
+
 public class UserDAO implements IDataAccessObject<User> {
-    
+    Connection connection;
+    UserDAO(Connection connection) {
+        this.connection = connection;
+    }
+
     public boolean createRecord(User data) {
+        try {
+            String sql = "INSERT INTO users (username, email, pass) VALUES (?, ?, ?)"; 
+            PreparedStatement stmnt = this.connection.prepareStatement(sql);
+            stmnt.setString(1, data.username);
+            stmnt.setString(2, data.email);
+            stmnt.setString(3, data.pass);
+            stmnt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
