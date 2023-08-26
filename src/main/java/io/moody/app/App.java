7351c5721dc15be3640c5f2861ac97ca0b261eac
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-25 20:07:16 -0700

    added javalin usage

--- src/main/java/io/moody/app/App.java
+++ src/main/java/io/moody/app/App.java
@@ -1,13 +1,16 @@
 package io.moody.app;
 
-import java.util.*;
+//import java.util.*;
 import java.io.IOException;
 import java.sql.*;
+import io.javalin.Javalin;
 
 public class App 
 {
     public static void main( String[] args )
     {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
         try (Connection connection = DatabaseUtil.getConnection();) {
             UserDAO userDAO = new UserDAO(connection);
             User newUser = new User("blehman3000", "suckthis@notanemail.com", "lickme123");
