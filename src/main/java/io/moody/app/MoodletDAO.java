d6a27df13af9e6ed83b0460aafeb85a49d7443a8
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-28 15:07:59 -0700

    updated MoodletDAO with new knowledge

--- src/main/java/io/moody/app/MoodletDAO.java
+++ src/main/java/io/moody/app/MoodletDAO.java
@@ -8,7 +8,7 @@ public class MoodletDAO implements IDAOMulti<Moodlet> {
     public boolean createRecord(List<Moodlet> data) {
         boolean success = false;
         try (Connection connection = DatabaseUtil.getConnection()) {
-            String sql = "INSERT INTO moodlet (user_id_fk, sleep_value, comfort_value, fun_value, hunger_value, social_vlaue, toilet_value, hygiene_value ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO moodlet VALUES (DEFAULT, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement ps = connection.prepareStatement(sql);
             for (int i = 0; i < data.size(); i++) {
                 Moodlet currentMoodlet = data.get(i);
