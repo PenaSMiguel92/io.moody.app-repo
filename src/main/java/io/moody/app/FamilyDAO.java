cd0da5183b6bb06898aa07036a1988a60a9d0027
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-29 17:14:40 -0700

    implemented createRecord for FamilyDAO

--- src/main/java/io/moody/app/FamilyDAO.java
+++ src/main/java/io/moody/app/FamilyDAO.java
@@ -1,11 +1,42 @@
 package io.moody.app;
 
+import java.io.IOException;
+import java.sql.*;
 import java.util.*;
 
 public class FamilyDAO {
 
     public boolean createRecord(List<User> family, User primaryUser) {
-        return false;
+        boolean success = false;
+        try (Connection connection = DatabaseUtil.getConnection();) {
+            Map<Integer, Long> map = new HashMap<>();
+            int current = 1;
+            String sql = "INSERT INTO user_family VALUES ";
+            
+            for (User member : family) {
+
+                sql += "(DEFAULT, ?, ?)";
+                map.put(current++, primaryUser.getId());
+                map.put(current++, member.getId());
+                String tail = current >= family.size() * 2 ? ";" : ",";
+                sql += tail;
+            }
+            PreparedStatement ps = connection.prepareStatement(sql);
+            map.forEach((key, value) -> {
+                try {
+                    ps.setLong(key, value);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected > 0)
+                success = true;
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+        return success;
     }
 
     public List<User> readRecord(long primaryUserId) {
