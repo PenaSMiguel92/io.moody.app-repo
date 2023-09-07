ad809f61678beb67f0d4212400c127a2695868a5
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-06 22:56:08 -0700

    productservice and controller implemented

--- src/main/java/io/moody/app/Controller.java
+++ src/main/java/io/moody/app/Controller.java
@@ -1,5 +1,13 @@
 package io.moody.app;
 
+import io.javalin.Javalin;
+
 public class Controller {
     
+    ProductService service = new ProductService();
+
+
+    public void setEndpoints(Javalin app) {
+        app.get("/products", ctx -> service.getAllProducts(ctx));
+    }
 }
--- src/main/java/io/moody/app/ProductService.java
+++ src/main/java/io/moody/app/ProductService.java
@@ -1,5 +1,20 @@
 package io.moody.app;
 
+import io.javalin.http.Context;
+import java.util.*;
+
 public class ProductService {
+    DataBase db = new DataBase();
+
+    public ProductService() {
+        db.setupDatabase();
+    }
     
+    public void getAllProducts(Context ctx) {
+        List<Product> products = this.db.getMyProducts();
+        ctx.json(products);
+
+        ctx.status(200);
+        
+    }
 }
