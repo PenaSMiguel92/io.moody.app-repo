407ffb1314c46800875340382ae2fabe61a2bfdd
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-06 22:08:30 -0700

    first part

--- src/main/java/io/moody/app/App.java
+++ src/main/java/io/moody/app/App.java
@@ -3,21 +3,23 @@ package io.moody.app;
 //import java.util.*;
 import java.io.IOException;
 import java.sql.*;
+import io.javalin.Javalin;
 
 public class App 
 {
     public static void main( String[] args )
     {
+        Javalin app = Javalin.create().get("/", ctx -> ctx.result("Hello World!")).start(8080);
         
-        try (Connection connection = DatabaseUtil.getConnection();) {
-            System.out.print("success");
-            // UserDAO userDAO = new UserDAO(connection);
-            // User newUser = new User("blehman3000", "suckthis@notanemail.com", "lickme123");
-            // userDAO.createRecord(newUser);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // try (Connection connection = DatabaseUtil.getConnection();) {
+        //     System.out.print("success");
+        //     // UserDAO userDAO = new UserDAO(connection);
+        //     // User newUser = new User("blehman3000", "suckthis@notanemail.com", "lickme123");
+        //     // userDAO.createRecord(newUser);
+        // } catch (SQLException e) {
+        //     e.printStackTrace();
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
     }
 }
